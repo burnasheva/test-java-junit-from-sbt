96317453a4c3e2906b8efc53a4fc90cc40d545e7
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-29 15:41:18 +0300

    add tests in different statuses

--- src/test/java/com/franklinchen/SimpleTest.java
+++ src/test/java/com/franklinchen/SimpleTest.java
@@ -7,6 +7,17 @@ import static org.junit.Assert.assertEquals;
 public class SimpleTest {
     @Test
     public void additionWorks() {
+        assertEquals(4, 2+2);
+    }
+
+    @Test
+    public void minusWorks() {
+        assertEquals(5, 2+2);
+    }
+
+    @Test
+    @Ignore
+    public void multiplicationWorks() {
         assertEquals(5, 2+2);
     }
 }
