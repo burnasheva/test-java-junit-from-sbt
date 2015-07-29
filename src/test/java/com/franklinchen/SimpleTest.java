53a92e654fa6b8e20251652be9bcc8a3433456bb
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-29 14:07:48 +0300

    add test to ignore

--- src/test/java/com/franklinchen/SimpleTest.java
+++ src/test/java/com/franklinchen/SimpleTest.java
@@ -1,10 +1,12 @@
 package com.franklinchen;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 public class SimpleTest {
     @Test
+    @Ignore
     public void additionWorks() {
         assertEquals(4, 2+2);
     }
