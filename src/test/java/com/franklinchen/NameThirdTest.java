d6c912e4081445839b00c3f00a78e20871cd8ca9
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-29 17:48:32 +0300

    all tests in ignore

--- src/test/java/com/franklinchen/NameThirdTest.java
+++ src/test/java/com/franklinchen/NameThirdTest.java
@@ -1,21 +1,24 @@
 package com.franklinchen;
 
 import org.junit.Test;
-
+import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 public class NameThirdTest {
     @Test
+    @Ignore
     public void addSome() {
         assertEquals(4, 2+2);
     }
 
     @Test
+    @Ignore
     public void minusSome() {
         assertEquals(3, 5-2);
     }
 
     @Test
+    @Ignore
     public void mulipleSome() {
         assertEquals(4, 2*2);
     }
--- src/test/java/com/franklinchen/SimpleTest2.java
+++ src/test/java/com/franklinchen/SimpleTest2.java
@@ -7,16 +7,19 @@ import static org.junit.Assert.assertEquals;
 
 public class SimpleTest2 {
     @Test
+    @Ignore
     public void additionWorks() {
         assertEquals(4, 2+2);
     }
 
     @Test
+    @Ignore
     public void minusWorks() {
         assertEquals(3, 5-2);
     }
 
     @Test
+    @Ignore
     public void multiplicationWorks() {
         assertEquals(4, 2*2);
     }
