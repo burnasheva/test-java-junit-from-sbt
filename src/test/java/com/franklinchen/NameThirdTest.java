2a9951b79a44ed2c64c38bb01abbd0b704797f91
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-29 16:25:09 +0300

    all tests passed

---
+++ src/test/java/com/franklinchen/NameThirdTest.java
@@ -0,0 +1,22 @@
+package com.franklinchen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NameThirdTest {
+    @Test
+    public void addSome() {
+        assertEquals(4, 2+2);
+    }
+
+    @Test
+    public void minusSome() {
+        assertEquals(3, 5-2);
+    }
+
+    @Test
+    public void mulipleSome() {
+        assertEquals(4, 2*2);
+    }
+}
